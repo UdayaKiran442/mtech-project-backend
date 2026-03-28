f1c0143ebbb137e0969679fe5a4f4b3483183384
UdayaKiran442 <72691871+UdayaKiran442@users.noreply.github.com>
2026-03-28 14:49:31 +0530

    chore: apply format document to schema.ts

--- src/repository/schema.ts
+++ src/repository/schema.ts
@@ -1,16 +1,20 @@
-import { boolean, index, integer, pgTable, primaryKey, timestamp, varchar } from "drizzle-orm/pg-core";
+import { boolean, index, pgTable, primaryKey, timestamp, varchar } from "drizzle-orm/pg-core";
 
-export const users = pgTable("users", {
-	userId: varchar("user_id").primaryKey(),
-	name: varchar("name").notNull(),
-	email: varchar("email").notNull().unique(),
-	passwordHash: varchar("password_hash").notNull(),
-	organisationId: varchar("organisation_id"),
-	createdAt: timestamp("created_at").defaultNow().notNull(),
-	updatedAt: timestamp("updated_at").defaultNow().notNull(),
-}, (users) => ({
-	emailIdx: index("email_idx").on(users.email),
-}));
+export const users = pgTable(
+	"users",
+	{
+		userId: varchar("user_id").primaryKey(),
+		name: varchar("name").notNull(),
+		email: varchar("email").notNull().unique(),
+		passwordHash: varchar("password_hash").notNull(),
+		organisationId: varchar("organisation_id"),
+		createdAt: timestamp("created_at").defaultNow().notNull(),
+		updatedAt: timestamp("updated_at").defaultNow().notNull(),
+	},
+	(users) => ({
+		emailIdx: index("email_idx").on(users.email),
+	}),
+);
 
 export const organisation = pgTable("organisation", {
 	organisationId: varchar("organisation_id").primaryKey(),
@@ -38,17 +42,21 @@ export const workspace = pgTable(
 	}),
 );
 
-export const workspaceMembers = pgTable("workspace_members", {
-	memberId: varchar("member_id").primaryKey(),
-	workspaceId: varchar("workspace_id").notNull(),
-	userId: varchar("user_id").notNull(),
-	role: varchar("role").notNull(),
-	createdAt: timestamp("created_at").defaultNow().notNull(),
-	updatedAt: timestamp("updated_at").defaultNow().notNull(),
-}, (workspaceMembers) => ({
-	workspaceIdIdx: index("workspace_id_idx_members").on(workspaceMembers.workspaceId),
-	userIdIdx: index("user_id_idx").on(workspaceMembers.userId),
-}));
+export const workspaceMembers = pgTable(
+	"workspace_members",
+	{
+		memberId: varchar("member_id").primaryKey(),
+		workspaceId: varchar("workspace_id").notNull(),
+		userId: varchar("user_id").notNull(),
+		role: varchar("role").notNull(),
+		createdAt: timestamp("created_at").defaultNow().notNull(),
+		updatedAt: timestamp("updated_at").defaultNow().notNull(),
+	},
+	(workspaceMembers) => ({
+		workspaceIdIdx: index("workspace_id_idx_members").on(workspaceMembers.workspaceId),
+		userIdIdx: index("user_id_idx").on(workspaceMembers.userId),
+	}),
+);
 
 export const invitations = pgTable("invitations", {
 	invitationId: varchar("invitation_id").primaryKey(),
@@ -59,42 +67,54 @@ export const invitations = pgTable("invitations", {
 	accepted: boolean("accepted").default(false).notNull(),
 	newUser: boolean("new_user").notNull(),
 	invitedAt: timestamp("invited_at").defaultNow().notNull(),
-})
+});
 
-export const knowledgeBase = pgTable("konwledge_base", {
-	fileId: varchar("file_id").primaryKey(),
-	workspaceId: varchar("workspace_id").notNull(),
-	fileUrl: varchar("file_url").notNull(),
-	key: varchar("key").notNull(), // s3 key of the file
-	uploadedBy: varchar("uploaded_by").notNull(), // id of the person who uploaded the file
-	uploadedAt: timestamp("uploaded_at").defaultNow().notNull(),	
-}, (knowledgeBase) => ({
-	workspaceIdIdx: index("workspace_id_idx_knowledge_base").on(knowledgeBase.workspaceId),
-}))
+export const knowledgeBase = pgTable(
+	"konwledge_base",
+	{
+		fileId: varchar("file_id").primaryKey(),
+		workspaceId: varchar("workspace_id").notNull(),
+		fileUrl: varchar("file_url").notNull(),
+		key: varchar("key").notNull(), // s3 key of the file
+		uploadedBy: varchar("uploaded_by").notNull(), // id of the person who uploaded the file
+		uploadedAt: timestamp("uploaded_at").defaultNow().notNull(),
+	},
+	(knowledgeBase) => ({
+		workspaceIdIdx: index("workspace_id_idx_knowledge_base").on(knowledgeBase.workspaceId),
+	}),
+);
 
 export const conversations = pgTable("conversations", {
 	conversationId: varchar("conversation_id").primaryKey(),
 	type: varchar("type").notNull(), // "dm" or "group"
 	createdAt: timestamp("created_at").defaultNow().notNull(),
-})
+});
 
-export const conversationMembers = pgTable("conversation_members", {
-	conversationId: varchar("conversation_id").notNull(),
-	userId: varchar("user_id").notNull(),
-	joinedAt: timestamp("joined_at").defaultNow().notNull(),
-}, (conversationMembers) => ({
-	conversationIdIdx: index("conversation_id_idx_conversation_members").on(conversationMembers.conversationId),
-	userIdIdx: index("user_id_idx_conversation_members").on(conversationMembers.userId),
-	primaryKey: primaryKey({columns: [conversationMembers.conversationId, conversationMembers.userId]})
-}))	
+export const conversationMembers = pgTable(
+	"conversation_members",
+	{
+		conversationId: varchar("conversation_id").notNull(),
+		userId: varchar("user_id").notNull(),
+		joinedAt: timestamp("joined_at").defaultNow().notNull(),
+	},
+	(conversationMembers) => ({
+		conversationIdIdx: index("conversation_id_idx_conversation_members").on(conversationMembers.conversationId),
+		userIdIdx: index("user_id_idx_conversation_members").on(conversationMembers.userId),
+		primaryKey: primaryKey({ columns: [conversationMembers.conversationId, conversationMembers.userId] }),
+	}),
+);
 
-export const messages = pgTable("messages", {
-	messageId: varchar("message_id").primaryKey(),
-	conversationId: varchar("conversation_id").notNull(),
-	senderId: varchar("sender_id").notNull(),
-	text: varchar("text").notNull(),
-	createdAt: timestamp("created_at").defaultNow().notNull(),
-}, (messages) => ({
-	conversationIdIdx: index("conversation_id_idx_messages").on(messages.conversationId),
-	senderIdIdx: index("sender_id_idx_messages").on(messages.senderId),
-}))
+export const messages = pgTable(
+	"messages",
+	{
+		messageId: varchar("message_id").primaryKey(),
+		conversationId: varchar("conversation_id").notNull(),
+		senderId: varchar("sender_id").notNull(),
+		text: varchar("text").notNull(),
+		createdAt: timestamp("created_at").defaultNow().notNull(),
+	},
+	(messages) => ({
+		conversationIdIdx: index("conversation_id_idx_messages").on(messages.conversationId),
+		senderIdIdx: index("sender_id_idx_messages").on(messages.senderId),
+	}),
+);
